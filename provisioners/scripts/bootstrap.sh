e2c6c3406b6f02bd0609bd64fc8de12a9df160f0
Mike Savage <mike.savage@gmail.com>
2022-08-11 16:28:19 -0400

    Revise script for RHEL 8.x

--- provisioners/scripts/bootstrap.sh
+++ provisioners/scripts/bootstrap.sh
@@ -4,27 +4,17 @@ set -ex
 sed -i "s/AABBccddeeff112233gghh/$api_token/g" ~/.ansible.cfg
 curl -L $satellite_manifest -o /tmp/manifest.zip
 
-#sudo yum -y install git
-#curl https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -o /tmp/epel-release-latest-7.noarch.rpm
-#sudo yum install -y /tmp/epel-release-latest-7.noarch.rpm
-#sudo yum install -y ansible
-#sudo yum remove -y epel-release
-#rm /tmp/epel-release-latest-7.noarch.rpm
-
-sudo yum-config-manager --enable rhel-7-server-rhui-optional-rpms
-sudo yum-config-manager --enable rhel-server-rhui-rhscl-7-rpms
 sudo yum -y install @development
-sudo yum -y install rh-python36
-#scl enable rh-python36 bash ### doesn't work within a bash script
-source /opt/rh/rh-python36/enable
+sudo yum -y install python39
+
 export PYVENV_PROJDIR="/tmp/ansible_venv"
 mkdir -p $PYVENV_PROJDIR
-python3.6 -m pip install --user --upgrade pip setuptools
-python3.6 -m venv $PYVENV_PROJDIR
+python3.9 -m pip install --user --upgrade pip setuptools
+python3.9 -m venv $PYVENV_PROJDIR
 source $PYVENV_PROJDIR/bin/activate
-python3.6 -m pip install --upgrade pip setuptools
-python3.6 -m pip install wheel
-python3.6 -m pip install \
+python3.9 -m pip install --upgrade pip setuptools
+python3.9 -m pip install wheel
+python3.9 -m pip install \
    ansible==2.9.27 \
    jmespath
 ansible-galaxy collection install -r /tmp/requirements.yml --force
