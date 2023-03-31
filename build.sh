7fafc9adec8a4fa0fe3d98296f3fe9406b549de9
Ryan Clements <rclement@redhat.com>
2023-03-31 09:10:57 +0000

    fix spacing

--- build.sh
+++ build.sh
@@ -24,14 +24,14 @@ BUILD_FILE="build_artifact-$(date +%Y-%m-%d.%H%M).txt"
 
 function log()
 {
-	if [[ "${1}" == "error" ]]
-	then
-    PRE="${DARK_GREY}[${RED}*${DARK_GREY}]${RESET}"
-		echo -e "${PRE} ${2}"
-	else
-    PRE="${DARK_GREY}[${GREEN}*${DARK_GREY}]${RESET}"
-    echo -e "${PRE} ${1}"
-  fi
+ if [[ "${1}" == "error" ]]
+ then
+   PRE="${DARK_GREY}[${RED}*${DARK_GREY}]${RESET}"
+   echo -e "${PRE} ${2}"
+ else
+   PRE="${DARK_GREY}[${GREEN}*${DARK_GREY}]${RESET}"
+   echo -e "${PRE} ${1}"
+ fi
 }
 
 echo -ne "
@@ -47,19 +47,19 @@ ${RESET}"
 # Check AWS creds
 
 if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
-	log "error" "AWS_ACCESS_KEY_ID is not defined."
-	log "error" \
-	 	  "Type: ${CYAN}export ${CYAN}AWS_ACCESS_KEY_ID${DARK_GREY}=${CYAN}YOURACCESSKEYHERE${NORMAL}"
-	echo -e "${DARK_GREY}"
-	exit 1
+  log "error" "AWS_ACCESS_KEY_ID is not defined."
+  log "error" \
+      "Type: ${CYAN}export ${CYAN}AWS_ACCESS_KEY_ID${DARK_GREY}=${CYAN}YOURACCESSKEYHERE${NORMAL}"
+  echo -e "${DARK_GREY}"
+  exit 1
 fi
 
 if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
-	log "error" "AWS_SECRET_ACCESS_KEY is not defined."
-	log "error" \
-	 	  "Type: ${CYAN}export ${CYAN}AWS_SECRET_ACCESS_KEY${DARK_GREY}=${CYAN}YOURSECRETKEYHERE${NORMAL}"
-	echo -e "${DARK_GREY}"
-	exit 1
+  log "error" "AWS_SECRET_ACCESS_KEY is not defined."
+  log "error" \
+      "Type: ${CYAN}export ${CYAN}AWS_SECRET_ACCESS_KEY${DARK_GREY}=${CYAN}YOURSECRETKEYHERE${NORMAL}"
+  echo -e "${DARK_GREY}"
+  exit 1
 fi
 
 log "Starting packer"
@@ -93,6 +93,4 @@ echo -e "Will start watching the output using: ${BLUE}${BOLD}tail -f log${RESET}
 
 sleep 5
 tail -f log
-
-
 
