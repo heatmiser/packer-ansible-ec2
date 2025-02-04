46db5d601c247eb817f53b3fe0fc10ad4d6b3204
Mike Savage <mike.savage@gmail.com>
2025-02-04 12:14:33 -0500

    Switch to ansible-core 2.15.13

--- provisioners/scripts/bootstrap.sh
+++ provisioners/scripts/bootstrap.sh
@@ -4,8 +4,8 @@ set -ex
 sed -i "s/AABBccddeeff112233gghh/$api_token/g" ~/.ansible.cfg
 curl -L $satellite_manifest -o /tmp/manifest.zip
 
-sudo yum -y install @development
-sudo yum -y install python39
+sudo dnf -y install @development
+sudo dnf -y install python39 python3-pip
 
 export PYVENV_PROJDIR="/tmp/ansible_venv"
 mkdir -p $PYVENV_PROJDIR
@@ -15,7 +15,7 @@ source $PYVENV_PROJDIR/bin/activate
 python3.9 -m pip install --upgrade pip setuptools
 python3.9 -m pip install wheel
 python3.9 -m pip install \
-   ansible-core==2.11.7 \
+   ansible-core==2.15.13 \
    jmespath
 ansible-galaxy collection install -r /tmp/requirements.yml --force
 history -c
